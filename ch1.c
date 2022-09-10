2c191ae84ecf6eeed51ad4fcf0ddc78627df0c37
ELKABLI <amina.elkabli1@gmail.com>
2022-09-10 16:54:19 +0200

    modifiez ch1

---
+++ ch1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+int main() {
+	int age, num;
+    char nom[10], prenom[20], sexe[10];
+    
+    printf("veuillez entrer votre nom");
+    scanf("%s",&nom);
+    printf("veuillez entrer votre prenom");
+    scanf("%s",&prenom);
+    printf("veuillez entrer votre sexe");
+    scanf("%s",&sexe);
+    printf("veuillez entrer votre age");
+    scanf("%d",&age);
+    printf("veuillez entrer votre num");
+    scanf("%d",&num);
+	return 0;
+}
