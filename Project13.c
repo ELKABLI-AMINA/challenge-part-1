1e623a2620f0f082b1c1bf8e0efd6fba7cb88f02
ELKABLI <amina.elkabli1@gmail.com>
2022-09-10 20:10:50 +0200

    modifiez Project13

---
+++ Project13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	
+                int var;
+                printf("veuillez entrer la valeur");
+                scanf("%d",&var);
+                printf("la valeur en octal est: %o",var);
+                printf("la valeur en hexadicimal est: %x",var );
+	return 0;
+}
